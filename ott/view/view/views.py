0a4b31657796c88c1c7a024da76a1cc9d249fe96
fpurcell <fxpurcell@gmail.com>
2012-12-20 01:03:28 -0800

    fix the here / parent stuff for locating static folders (with move of stuff to views folder)

--- ott/view/view/views.py
+++ ott/view/view/views.py
@@ -9,9 +9,13 @@ from pyramid.config import Configurator
 from pyramid.session import UnencryptedCookieSessionFactoryConfig
 from pyramid.response import Response
 from pyramid.config import Configurator
+from pyramid.events import NewRequest
+from pyramid.events import subscriber
+from pyramid.events import ApplicationCreated
 
 import ott.view.view.desktop
 import ott.view.view.mobile
+
 
 @view_config(context='pyramid.exceptions.NotFound', renderer='notfound.mako')
 def notfound_view(self):
@@ -36,23 +40,77 @@ def index_view(request):
     return {'authenticated':auth, 'authorized':perm}
 
 
-def make_views(config):
+
+@subscriber(ApplicationCreated)
+def application_created_subscriber(event):
+    '''
+       what do i do?
+
+       1. I'm called at startup of the Pyramid app.  
+       2. I could be used to make db connection (pools), etc...
+    '''
+    log.info('Starting pyramid server -- visit me on http://localhost:8080')
+
+
+@subscriber(NewRequest)
+def new_request_subscriber(event):
+    '''
+       what do i do?
+
+       1. entry point for a new server request
+       2. configure the request context object (can insert new things like db connections or authorization to pass around in this given request context)
+    '''
+    log.debug("new request called -- request is 'started'")
+    request = event.request
+    request.model = get_model()
+    settings = request.registry.settings
+    request.add_finished_callback(cleanup)
+
+
+def cleanup(request):
+    '''
+       what do i do?
+
+       1. I was configured via the new_request_subscriber(event) method
+       2. I'm called via a server event (when a request is 'finished')
+       3. I could do random cleanup tasks like close database connections, etc... 
+    '''
+    log.debug("cleanup called -- request is 'finished'")
+
+
+MODEL_GLOBAL = None
+def get_model():
+    ''' @see make_views() below, which should have a model passed in to configure the model global 
+    '''
+    global MODEL_GLOBAL
+    if MODEL_GLOBAL is None:
+        # do something to create a model...
+        MODEL_GLOBAL = {}
+
+    return MODEL_GLOBAL
+
+
+def make_views(config, model):
     ''' adds the views (see below) and static directories to pyramid's config
         TODO: is there a better way to dot this (maybe via an .ini file)
     '''
+
+    # set the global model to this mod
+    global MODEL_GLOBAL
+    MODEL_GLOBAL = model
+
     # important ... allow .html extension on mako templates
     config.add_renderer(".html", "pyramid.mako_templating.renderer_factory")
 
-
     # routes setup
-    config.add_route('index',  '/')
-    config.add_route('find_stop', '/find_stop')
-    config.add_route('mfind_stop', '/mobile/find_stop')
+    config.add_route('index',       '/')
+    config.add_route('find_stop',   '/find_stop')
+    config.add_route('mfind_stop',  '/mobile/find_stop')
 
-    here = os.path.dirname(os.path.abspath(__file__))
-    config.add_static_view('css',    os.path.join(here, 'static/css'))
-    config.add_static_view('js',     os.path.join(here, 'static/js'))
-    config.add_static_view('images', os.path.join(here, 'static/images'))
+    here   = os.path.dirname(os.path.abspath(__file__))
+    parent = os.path.abspath(os.path.join(here, os.path.pardir))
+    images = os.path.join(parent, 'static/images')
+    config.add_static_view('images', images)
 
     config.scan()
 
@@ -76,3 +134,4 @@ def make_config(settings):
     config.add_subscriber('ott.view.locale.subscribers.add_localizer', 'pyramid.events.NewRequest')
 
     return config
+
--- ott/view/view_app.py
+++ ott/view/view_app.py
@@ -1,9 +1,6 @@
 import os
 import logging
 
-from pyramid.events import NewRequest
-from pyramid.events import subscriber
-from pyramid.events import ApplicationCreated
 from wsgiref.simple_server import make_server
 
 from ott.view.view.views import make_views
@@ -20,43 +17,6 @@ log = logging.getLogger(__file__)
 log.setLevel(logging.INFO)
 
 
-@subscriber(ApplicationCreated)
-def application_created_subscriber(event):
-    '''
-       what do i do?
-
-       1. I'm called at startup of the Pyramid app.  
-       2. I could be used to make db connection (pools), etc...
-    '''
-    log.info('Starting pyramid server -- visit me on http://localhost:8080')
-
-
-@subscriber(NewRequest)
-def new_request_subscriber(event):
-    '''
-       what do i do?
-
-       1. entry point for a new server request
-       2. configure the request context object (can insert new things like db connections or authorization to pass around in this given request context)
-    '''
-    log.debug("new request called -- request is 'started'")
-    request = event.request
-    request.model = model
-    settings = request.registry.settings
-    request.add_finished_callback(cleanup)
-
-
-def cleanup(request):
-    '''
-       what do i do?
-
-       1. I was configured via the new_request_subscriber(event) method
-       2. I'm called via a server event (when a request is 'finished')
-       3. I could do random cleanup tasks like close database connections, etc... 
-    '''
-    log.debug("cleanup called -- request is 'finished'")
-
-
 def main():
     # configuration settings
     here = os.path.dirname(os.path.abspath(__file__))
@@ -67,8 +27,8 @@ def main():
     settings={}
     settings['mako.directories'] = mako_dir
     config=make_config(settings)
-    make_views(config)
-    config.scan()
+    make_views(config, model)
+
 
     # serve app
     app = config.make_wsgi_app()
