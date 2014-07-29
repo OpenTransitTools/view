28161d7488e2f0f5d6eee3f6d3c5f126f3311974
Frank Purcell <fxpurcell@gmail.com>
2014-07-29 11:25:51 -0700

    refactor ... dao objects are now what's delt with ... no mo SOLR

--- ott/view/static/js/test/autocomplete_saved_searches.js
+++ ott/view/static/js/test/autocomplete_saved_searches.js
@@ -16,11 +16,42 @@ function PlaceDAO(label, lat, lon, saved)
     this.set(label, lat, lon, saved);
 }
 
-function SolrPlaceDAO(solr_rec, saved)
+/** extended data and functionality for SOLR*/
+function SolrPlaceDAO(doc, saved)
 {
-    this.set(solr_rec.label, solr_rec.solr_doc.lat, solr_rec.solr_doc.lon, saved);
-    this.city  = solr_rec.solr_doc.city;
-    this.type  = solr_rec.solr_doc.type;
+    /** 
+     * function that controls the naming of the geo point
+     * NOTE: this isn't localized (e.g., type name and city conjunction), so override in .mako
+     *
+     * @return: formatted place name, ala 844 SE X Street, Portland -or- A Ave, Portalnd (Stop ID 2)  
+     */
+    this.place_name_format = function(doc)
+    {
+        var ret_val = doc.name;
+        try {
+            var city = '';
+            if (doc.city && doc.city.length > 0)
+               city = ", " + doc.city;
+
+            var id = doc.id;
+            if (doc.type == 'stop')
+                id = doc.stop_id;
+
+            var stop = '';
+            if(doc.type == 'stop')
+                stop = " (Stop ID " + id + ")";
+            ret_val = doc.name + city + stop;
+        }
+        catch(e) {
+            console.log(e);
+        }
+        return ret_val;
+    };
+
+    var label = this.place_name_format(doc);
+    this.set(label, doc.lat, doc.lon, saved);
+    this.city = doc.city;
+    this.type = doc.type;
 }
 SolrPlaceDAO.prototype = new PlaceDAO();
 
@@ -54,7 +85,6 @@ function SavedSearches(removeTitle)
             console.log(e);
         }
     };
-
 
 
    /**
@@ -129,8 +159,7 @@ function SavedSearches(removeTitle)
         a.onclick = function(e) {
             if(item && item.saved === false)
             {
-                var rec = new SolrPlaceDAO(item);
-                THIS.add(rec);
+                THIS.add(item);
             }
         };
 
@@ -178,27 +207,6 @@ function SOLRAutoComplete(input_div, solr_url, num_results)
         console.log('Selected:' + sel.value + ", id: " + sel.id + " " + this.solr_url);
     };
 
-    /** 
-     * function that controls the naming of the geo point
-     * NOTE: this isn't localized (e.g., type name and city conjunction), so override in .mako
-     *
-     * @return: formatted place name, ala 844 SE X Street, Portland -or- A Ave, Portalnd (Stop ID 2)  
-     */
-    this.place_name_format = function(name, city, type, id)
-    {
-        var ret_val = name;
-        try {
-            var stop = '';
-            if(type == 'stop')
-                stop = " (Stop ID " + id + ")";
-            ret_val = name + city + stop;
-        }
-        catch(e) {
-            console.log(e);
-        }
-        return ret_val;
-    };
-
 
     this.enable_ajax = function()
     {
@@ -227,41 +235,22 @@ function SOLRAutoComplete(input_div, solr_url, num_results)
                         //push saved searchings 
                         var data = [];
 
+                        // step 1: get cached searches
                         var save_list = THIS.cache.find(request.term);
                         //data.concat(save_list);
                         data = save_list;
 
-                        // step 0: SOLR elements...
+                        // step 2: SOLR elements...
                         docs = resp.response.docs;
                         len = docs.length;
 
-                        // step 1: loop through SOLR results, and build data object required for jQ's autocomplete 
-
-                        for (var i = 0; i < len; i++)
+                        // step 3: loop through SOLR results, and build data object required for jQ's autocomplete 
+                        for(var i = 0; i < len; i++)
                         {
-                            var doc = docs[i];
-
-                            // step 2: make a label out of name, type_name and optionally city and stop id
-                            var city = '';
-                            if (doc.city && doc.city.length > 0)
-                                city = ", " + doc.city;
-
-                            var id = doc.id;
-                            if (doc.type == 'stop')
-                                id = doc.stop_id;
-
-                            var label = THIS.place_name_format(doc.name, city, doc.type, id);
-                             
-                            // step 3: make the autocomplete object, and add it to our return array
-                            var s = {
-                                "id"       : doc.id,
-                                "label"    : label,
-                                "value"    : label,
-                                "solr_doc" : doc,
-                                "saved"    : false
-                            };
-                            data.push(s);
+                            var dao = new SolrPlaceDAO(docs[i]);
+                            data.push(dao);
                         }
+
                         // step 4: jQuery UI autocomplete (required) callback
                         response(data);
                     }
