641fd96b127b2b30e54e82013ad79fe81aed4d74
Frank Purcell <fxpurcell@gmail.com>
2015-07-08 16:15:55 -0700

    fix the more tab

--- ott/view/utils/schedule_tabs.py
+++ ott/view/utils/schedule_tabs.py
@@ -28,15 +28,12 @@ def get_tabs(request, url, show_yesterday):
         day   = html_utils.get_first_param_as_int(request, 'day')
         date  = date_utils.set_date(date, month, day)
 
-    # get the more button value
     more = html_utils.get_first_param(request, 'more')
-    if more is None:
-        more = False
 
     ret_val = {}
     ret_val['more_form']   = date_utils.get_day_info(date)
     ret_val['pretty_date'] = date_utils.pretty_date(date)
-    ret_val['tabs'] = get_svc_date_tabs(date, url, more, get_translator(request))
+    ret_val['tabs'] = get_svc_date_tabs(date, url, more is not None, get_translator(request))
 
     return ret_val
 
