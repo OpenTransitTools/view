fc6674c7fd13e5743669f970ad2480e8a5f374e5
Frank Purcell <fxpurcell@gmail.com>
2017-10-02 14:11:36 -0700

    clean up unit tests ... test for mailto

--- ott/view/tests/tests_view.py
+++ ott/view/tests/tests_view.py
@@ -5,18 +5,17 @@ class ViewTests(OttTestCase):
 
     def test_stops_near(self):
         for m in ['', 'm/']:
-            ''' '''
             # test place and placeCoord
             url = self.get_url(m + 'stops_near.html', 'placeCoord=45.5,-122.5&place=XTCvsAdamAnt')
             s = self.call_url(url)
-            self.assertRegexpMatches(s,"miles away")
-            self.assertRegexpMatches(s,"SE Division")
-            self.assertRegexpMatches(s,"XTCvsAdamAnt")
+            self.assertRegexpMatches(s, "miles away")
+            self.assertRegexpMatches(s, "SE Division")
+            self.assertRegexpMatches(s, "XTCvsAdamAnt")
 
             # test named coord
             url = self.get_url(m + 'stops_near.html', 'place=834 SE LAMBERT ST::45.468602,-122.657627')
             s = self.call_url(url)
-            self.assertRegexpMatches(s,"miles away")
+            self.assertRegexpMatches(s, "miles away")
 
             # test named coord w/ city
             url = self.get_url(m + 'stops_near.html', 'place=834 SE LAMBERT ST::45.468602,-122.657627::Portland')
@@ -55,7 +54,7 @@ class ViewTests(OttTestCase):
         for m in ['', 'm/']:
             url = self.get_url(m + 'stop_select_form.html')
             s = self.call_url(url)
-            self.assertRegexpMatches(s,"MAX Blue")
+            self.assertRegexpMatches(s, "MAX Blue")
 
     def test_stop_select_list(self):
         ''' route stops ws: stop select for each route direction '''
@@ -82,7 +81,7 @@ class ViewTests(OttTestCase):
             for t in ['&sort=destination', '&sort=time']:
                 url = self.get_url(m + 'stop_schedule.html', 'stop_id=2' + t)
                 s = self.call_url(url)
-                self.assertRegexpMatches(s,"Lake Oswego")
+                self.assertRegexpMatches(s, "Lake Oswego")
 
                 url = self.get_url(m + 'stop_schedule.html', 'stop_id=2&more' + t)
                 s = self.call_url(url)
@@ -96,24 +95,22 @@ class ViewTests(OttTestCase):
             self.assertRegexpMatches(s, 'type="text" id="going" name="to" value="ZOO"')
 
     def test_plan_trip(self):
+        url = self.ini.get('ott.host_url', 'app:main')
+        mailto = "mailto.*{}.*planner.html".format(url.replace(":", "%3A"))
         for m in ['', 'm/']:
             url = self.get_url(m + 'planner.html', 'to=pdx::45.587546,-122.592925&from=zoo::45.5092,-122.7133&Hour=9&Minute=0&AmPm=pm')
             s = self.call_url(url)
             self.assertRegexpMatches(s, "MAX Red Line")
-            self.assertRegexpMatches(s, "MAX Red Line")
-            self.assertNotRegexpMatches(s, "localhost")
-            self.assertNotRegexpMatches(s, self.port)
+            self.assertRegexpMatches(s, mailto)
 
     def test_plan_walk(self):
         for m in ['', 'm/']:
             url = self.get_url(m + 'planner_walk.html', 'mode=WALK&from=pdx::45.587546,-122.592925&to=zoo')
             s = self.call_url(url)
-            self.assertRegexpMatches(s,"Airport Way")
-            self.assertNotRegexpMatches(s, "localhost")
-            self.assertNotRegexpMatches(s, self.port)
+            self.assertRegexpMatches(s, "Airport Way")
 
     def test_map_place(self):
         for m in ['', 'm/']:
             url = self.get_url(m + 'map_place.html', 'name=834 SE MILL ST&city=Portland&lon=-122.65705&lat=45.509865')
             s = self.call_url(url)
-            self.assertRegexpMatches(s,"Plan a trip to 834 SE MILL ST")
+            self.assertRegexpMatches(s, "Plan a trip to 834 SE MILL ST")
